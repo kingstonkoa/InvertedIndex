9221f8bb0ae026ed68f8bb5ff1e2a96349d5be60
unknown <johncaingles@gmail.com>
2015-11-01 01:00:17 +0800

    added StemmerWordDirectory class

---
+++ model/StemmerWordDirectory.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.ArrayList;
+
+public class StemmerWordDirectory {
+	private ArrayList<String> stopWords;
+
+	public ArrayList<String> getStopWords() {
+		return stopWords;
+	}
+
+	public void setStopWords(ArrayList<String> stopWords) {
+		this.stopWords = stopWords;
+	}
+}
