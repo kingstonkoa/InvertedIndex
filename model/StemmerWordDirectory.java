441319d771b56d4e6e2f87f25f49790621f37be7
unknown <johncaingles@gmail.com>
2015-11-01 23:31:20 +0800

    cleaned txtreader

--- model/StemmerWordDirectory.java
+++ model/StemmerWordDirectory.java
@@ -3,10 +3,17 @@ package model;
 import java.util.ArrayList;
 
 public class StemmerWordDirectory {
+	
 	private ArrayList<String> stopWords;
 	
-	
-	
+	public StemmerWordDirectory() {
+		initializeStopWords();
+	}
+		
+	private void initializeStopWords() {
+		
+	}
+
 	public ArrayList<String> getStopWords() {
 		return stopWords;
 	}
--- utilities/TxtReader.java
+++ utilities/TxtReader.java
@@ -16,37 +16,34 @@ public class TxtReader
     private int numOfDocuments = 303;
     StringBuilder stringBuilder = new StringBuilder();
     ArrayList<String> documentsAL = new ArrayList<>();
-    public TxtReader()
-    {
-   for(int i = 0; i < numOfDocuments; i++)
-   {
-   try{
-
-  FileInputStream fstream = new FileInputStream("src/resources/Tagalog News - "+(i+1)+".txt");
-
-  DataInputStream in = new DataInputStream(fstream);
-      BufferedReader br = new BufferedReader(new InputStreamReader(in));
-  String strLine;
-
-  while ((strLine = br.readLine()) != null)   {
-
-      stringBuilder.append(strLine);
-  }
-  //Close the input stream
-  in.close();
-  String finalString = stringBuilder.toString();
-  documentsAL.add(finalString);
-
-  //System.out.println(finalString);
-  }catch (Exception e){//Catch exception if any
-    System.err.println("Error: " + e.getMessage());
-  }
-   }
+    
+    public TxtReader() {
+    	for(int i = 0; i < numOfDocuments; i++) {
+			try{
+			  FileInputStream fstream = new FileInputStream("src/resources/Tagalog News - "+(i+1)+".txt");
+			
+			  DataInputStream in = new DataInputStream(fstream);
+			  BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			  String strLine;
+	
+			  while ((strLine = br.readLine()) != null){
+			      stringBuilder.append(strLine);
+			  }
+			  //Close the input stream
+			  in.close();
+			  String finalString = stringBuilder.toString();
+			  documentsAL.add(finalString);
+			
+			  //System.out.println(finalString);
+			  }catch (Exception e){//Catch exception if any
+			    System.err.println("Error: " + e.getMessage());
+			  }
+    	}
     }
     
 
     public ArrayList<String> getDocumentsList()
     {
-    return documentsAL;
+    	return documentsAL;
     }
 }
